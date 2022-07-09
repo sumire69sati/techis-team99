979deb843c674f320bb25dbc320e3a43af0df335
nagasaki1702 <m.nagasaki2012@gmail.com>
2022-07-09 12:46:16 +0900

    20220709n

---
+++ database/migrations/2022_07_03_103322_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('name')->length(100);
+            $table->integer('status')->default('1');
+            $table->smallInteger('type')->nullable();
+            $table->text('detail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
