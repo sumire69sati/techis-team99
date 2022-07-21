5c559d1fa4e5ff75c861d59b9c58d08a27140bb2
katsu84mt <katsu84mt@gmail.com>
2022-07-21 16:56:41 +0900

    22-07-21h

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/member-form',[App\Http\Controllers\AccountController::class,'create
 Route::post('/member',[App\Http\Controllers\AccountController::class,'store'])->name('member');
 Route::get('/login-form',[App\Http\Controllers\AccountController::class,'loginform'])->name('login-form');
 Route::post('/login',[App\Http\Controllers\AccountController::class,'login'])->name('login');
-Route::get('/home',[App\Http\Controllers\AccountController::class,'home'])->name('home');
+//Route::get('/home',[App\Http\Controllers\AccountController::class,'home'])->name('home');
 Route::get('/home-user',[App\Http\Controllers\AccountController::class,'homeuser'])->name('home-user');
 Route::get('/logout',[App\Http\Controllers\AccountController::class,'logout'])->name('logout');
 
