0d92be244cd130ec9b3b34efa39d87edcdb7cc45
sumire69sati <ysk26miwa441@gmail.com>
2022-07-22 15:08:36 +0900

    20200722アクセス制御の修正

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,17 @@ Route::get('/member-form',[App\Http\Controllers\AccountController::class,'create
 Route::post('/member',[App\Http\Controllers\AccountController::class,'store'])->name('member');
 Route::get('/login-form',[App\Http\Controllers\AccountController::class,'loginform'])->name('login-form');
 Route::post('/login',[App\Http\Controllers\AccountController::class,'login'])->name('login');
-Route::middleware('auth')->group(function(){
+
+Route::group(['middleware' => ['auth','can:admin-higher']],function(){
+    //top ユーザー一覧画面
+    Route::get('/user', [App\Http\Controllers\UserController::class, 'index']);
+    Route::get('/item_create', [App\Http\Controllers\ItemController::class, 'item_create'])->name('item_create');
+    
+});
+
+Route::group(['middleware' => ['auth','can:user-higher']],function(){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
-Route::get('/logout',[App\Http\Controllers\AccountController::class,'logout'])->name('logout');
-
-//top ユーザー一覧画面
-Route::get('/user', [App\Http\Controllers\UserController::class, 'index']);
 
 //edit 管理者権限付与画面への遷移(編集)
 Route::get('/user/edit/{id}', [App\Http\Controllers\UserController::class, 'edit']);
@@ -38,7 +42,6 @@ Route::post('/user/update',[App\Http\Controllers\UserController::class,'update']
 Route::get('/userDelete/{id}',[App\Http\Controllers\UserController::class,'userDelete']);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Route::get('/item_create', [App\Http\Controllers\ItemController::class, 'item_create'])->name('item_create');
 Route::post('/item_store', [App\Http\Controllers\ItemController::class, 'item_store'])->name('item_store');
 
 Route::get('/item_edit/{id}', [App\Http\Controllers\ItemController::class, 'item_edit'])->name('item_edit');
@@ -51,6 +54,9 @@ Route::get('/item_bottoms', [App\Http\Controllers\ItemController::class, 'item_b
 
 Route::get('/item_search', [App\Http\Controllers\ItemController::class, 'item_search'])->name('item_search');
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Route::get('/logout',[App\Http\Controllers\AccountController::class,'logout'])->name('logout');
+
 
 
 
