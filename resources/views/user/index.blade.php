187732257fa0a74e0141c00075f1169dd256e052
izumi170 <yama.izu.sev17@gmail.com>
2022-07-25 16:34:10 +0900

    20220725Yheader

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,15 +7,38 @@
 
         <!-- Bootstrap CSSの読み込み -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
 
-    </head>
     <body>
-        <!-- ログアウトボタン -->
-        <div style="width: 500px; margin: 100px auto;">
-            <div style="text-align:right;">
-            <a href="/logout" class="btn btn-secondary btn-sm">ログアウト</a>
+    <div class="manu px-md-0">   
+    <nav class="navbar navbar-expand-md bg-light rounded mb-3 border d-box">
+        <div class="header d-flex w-100 justify-content-between p-2">
+            <h1 class="text-muted">商品管理システム</h1>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                    <li class="decoration-none"><a  href="{{ route('logout') }}">ログアウト</a></li>
+                </ul>
+            </div>
         </div>
-
+        @if(Auth::user()->admine_id === 1)
+        <div class="container-fluid">
+            <button class="navbar-toggler btn-outline-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#Navbar" aria-controls="Navbar" aria-expanded="false" aria-label="ナビゲーションの切替">
+                メニュー
+            </button>
+            <div class="collapse navbar-collapse" id="Navbar">
+                <ul class="navbar-nav text-md-center nav-justified w-100">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">商品一覧</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('item_create') }}">商品登録</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ url('/user') }}">ユーザー一覧</a></li>
+                </ul>
+            </div>
+        </div>
+        @endif
+    </nav>
+</div>
             <!-- ユーザー一覧 -->
         <div class="row justify-content-center">
             <!-- table-hoverでマウスにカーソルを合わせると色が変わるようにしたが反応無し -->
